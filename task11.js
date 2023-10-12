deeedeef52935d840d8aa59d67ede515b910864f
Michael <bloodbath9th@gmail.com>
2023-10-12 22:06:06 +0300

    11 задание

---
+++ task11.js
@@ -0,0 +1,9 @@
+const second = 3665;
+
+const hours = Math.floor(second / 3600);
+
+const minute = Math.floor((second - hours * 3600) / 60);
+
+const secresult = (second - hours * 3600) - minute * 60;
+
+console.log(hours + ' ч\n' +  minute + ' м\n' + secresult + ' с\n');
