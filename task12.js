8e28934b5b52eb2dee27ca7281c86f67fbb540b3
Michael <bloodbath9th@gmail.com>
2023-10-12 22:09:58 +0300

    12 задание

---
+++ task12.js
@@ -0,0 +1,6 @@
+const a = 20;
+const b = 15;
+const c = 17;
+const perimetr = a + b + c;
+const polyper = perimetr/2;
+console.log(Math.sqrt(polyper * (polyper - a) * (polyper - b) * (polyper - c) ));
